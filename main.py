c4f8557fbb71da8b77c87196b5f5535d31de97b5
BentleyBot <winstonwilliamsiii@gmail.com>
2026-06-18 23:48:02 -0400

    refactor: remove redundant notifications router import and payback router inclusion

--- main.py
+++ main.py
@@ -4,6 +4,3 @@ from src.api.payback import router as payback_router
 app.include_router(payback_router)
 
 __all__ = ["app"]
-from api import payback, notifications
-app.include_router(payback.router)
-app.include_router(notifications.router)
