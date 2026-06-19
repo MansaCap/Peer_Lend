923f602f3ad173a470a02c5ab6b2dc78c8958915
BentleyBot <winstonwilliamsiii@gmail.com>
2026-06-18 21:44:42 -0400

    feat: include notifications and payback routers in the main application

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from src.api import app
 __all__ = ["app"]
 from src.api import app
 __all__ = ["app"]
+from api import notifications, payback
+
+app.include_router(notifications.router)
+app.include_router(payback.router)
